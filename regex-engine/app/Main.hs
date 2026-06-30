635c99e495170d76d121841eef0c49f667741feb
Przemysław Pindral <przemyslaw.pindral1@gmail.com>
2026-06-30 21:47:29 +0200

    feat: update Main module

--- regex-engine/app/Main.hs
+++ regex-engine/app/Main.hs
@@ -1,33 +1,51 @@
 module Main (main) where
 import System.Environment (getArgs)
-import Parser 
+import Parser
 import AST
 import Match
 import DFA
+import qualified Data.Map as Map
 
 checkMatch :: Bool -> IO ()
-checkMatch isMatch = do 
-    case isMatch of 
-        True -> putStrLn "MATCH!"
+checkMatch isMatch = do
+    case isMatch of
+        True  -> putStrLn "MATCH!"
         False -> putStrLn "NO MATCH!"
+
+showMinComparison :: Regex -> IO ()
+showMinComparison ast = do
+    let dfa    = buildDFA ast
+        minDfa = minimizeDFA dfa
+        before = Map.size (dfaEdges dfa)
+        after  = Map.size (dfaEdges minDfa)
+    putStrLn "DFA size comparison:"
+    putStrLn ("  subset construction : " ++ show before ++ " states")
+    putStrLn ("  after minimization  : " ++ show after  ++ " states")
+    putStrLn ("  reduced by          : " ++ show (before - after) ++ " states")
 
 main :: IO ()
 main = do
     args <- getArgs
-    let isDfa = "--dfa" `elem` args
-        rest = filter (/= "--dfa") args
-    if length rest /= 2 
-        then (do 
-                putStrLn "Invalid number of arguments"  
-                putStrLn "Ex: stack run -- \"(a|b)*c\" \"ababc\"")
-        else 
-            let pattern = rest !! 0 
-                input = rest !! 1
-                eth = parseWithErr pattern 
-            in (do case eth of 
-                    Left (_, errMsg) -> putStr (errMsg)
-                    Right ast -> (do 
-                        putStrLn ("pretty AST: " ++ pretty ast)
-                        if isDfa 
-                            then checkMatch $ matchDFA ast input 
-                            else checkMatch $ matches ast input))
+    let useDfa = "--dfa" `elem` args
+        useMin = "--min" `elem` args
+        rest   = filter (`notElem` ["--dfa", "--min"]) args
+    if length rest /= 2
+        then do
+            putStrLn "Invalid number of arguments"
+            putStrLn "Ex: stack run -- \"(a|b)*c\" \"ababc\"          (NFA engine)"
+            putStrLn "    stack run -- --dfa \"(a|b)*c\" \"ababc\"   (DFA engine)"
+            putStrLn "    stack run -- --min \"(a|b)*c\" \"ababc\"   (minimized DFA + size comparison)"
+        else do
+            let pattern = rest !! 0
+                input   = rest !! 1
+            case parseWithErr pattern of
+                Left (_, errMsg) -> putStr errMsg
+                Right ast -> do
+                    putStrLn ("pretty AST: " ++ pretty ast)
+                    if useMin
+                        then do
+                            showMinComparison ast
+                            checkMatch (matchMinDFA ast input)
+                        else if useDfa
+                            then checkMatch (matchDFA ast input)
+                            else checkMatch (matches ast input)
